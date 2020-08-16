a36d13dd4f923cc68cbd1aa1714f60bc3961ff87
Warren Jitsing <warren.jitsing@gmail.com>
2020-08-16 10:24:15 +0200

    (Add) Connection class init parameter check
    Why: Raise an error for invalid parameters passed to init in the event
         the users does not run MyPy or a static checker
    
    How: Passed parameter types are checked against a list of expected types
    
    Tags: connection check parameters

--- algo/client/connection.py
+++ algo/client/connection.py
@@ -4,6 +4,7 @@ Wraps the :mod:`socket` interface for low-level communication
 with the algo server.
 """
 import socket
+from typing import Dict, Any
 
 
 class Connection:
@@ -11,3 +12,12 @@ class Connection:
     def __init__(self, host: str, port: int):
         self.host: str = host
         self.port: int = port
+        self.check_valid()
+
+    def check_valid(self):
+        arg_types: Dict[str, type] = {"host": str, "port": int}
+        host_fine: bool = isinstance(self.host, arg_types.get("host"))
+        port_fine: bool = isinstance(self.port, arg_types.get("port"))
+        if not (host_fine and port_fine):
+            raise TypeError("Host or port invalid")
+
