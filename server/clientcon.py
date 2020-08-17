56cd59d16fea5c1392fdb008af0995f7fe6529b2
Warren Jitsing <warren.jitsing@gmail.com>
2020-08-17 11:21:22 +0200

    (Add) ClientConnection class to server
    Why: Handle the low-level socket communication that happens between
         client and server. Receive requests from and send back data to client
    
    How: socket
    
    Tags: server client connection

---
+++ server/clientcon.py
@@ -0,0 +1,18 @@
+"""
+    Low level communication with clients
+    -------------------------------------------------------------
+
+    Wraps the :mod:`socket` interface for low-level communication
+    with the :mod:`server`.
+"""
+import socket
+
+
+class ClientConnection:
+    """
+    An instance representing a client connection. Handles low-level
+    socket communication, receiving requests and sending back data.
+    """
+
+    def __init__(self) -> None:
+        pass
